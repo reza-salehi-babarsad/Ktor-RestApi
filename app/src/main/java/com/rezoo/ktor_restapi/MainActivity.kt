15ea4a6e4fa3d3a9ef050e7eb9d534d88cb59537
reza <rezasalehibabarsad@hotmail.com>
2023-05-21 12:50:01 +0330

    Initial commit

---
+++ app/src/main/java/com/rezoo/ktor_restapi/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rezoo.ktor_restapi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rezoo.ktor_restapi.ui.theme.KtorRestApiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KtorRestApiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KtorRestApiTheme {
+        Greeting("Android")
+    }
+}
